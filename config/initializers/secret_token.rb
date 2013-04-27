989ad7e902843cf2fbca62d6ad21d5d90d94625e
Sandip Trivedi <trivedi.sandip@gmail.com>
2013-04-27 15:27:58 -0400

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TcDisrupt13::Application.config.secret_token = '2f2efef653d96ba9a0e152a65da5517659c966db1bc96ef964ac63043f26e9d414a72524c851227321da51f4f4b5ff860d683d67f22794413c6aa84d6de6ea3e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+TcDisrupt13::Application.config.session_store :cookie_store, key: '_tc-disrupt-13_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# TcDisrupt13::Application.config.session_store :active_record_store
