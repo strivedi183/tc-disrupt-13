0edb91a39ced694f46c0c42e50548f83f566c13c
Sandip Trivedi <trivedi.sandip@gmail.com>
2013-04-28 05:45:40 -0400

    Added links to the twitter body

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def parse_tweet(tweet)
+    tweet = tweet.split(' ')
+    new_tweet = []
+    tweet.each do |phrase|
+      if phrase.include?('http')
+        @phrase = phrase
+      else
+        new_tweet << phrase
+      end
+    end
+    tweet = new_tweet.join(' ')
+  end
 end
