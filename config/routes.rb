6f279278c0b4d42ebbf36fb9846d8f1b3d56eaed
Sandip Trivedi <trivedi.sandip@gmail.com>
2013-04-28 01:14:51 -0400

    Started up the views and login

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 TcDisrupt13::Application.routes.draw do
   root :to => 'welcome#index'
+  resources :users, :permissions, :filters, :events, :contents
+  get '/login' => 'session#new'
+  post '/login' => 'session#create'
+  delete '/login' => 'session#destroy'
 end
