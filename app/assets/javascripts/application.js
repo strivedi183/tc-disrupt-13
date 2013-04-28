b6c6960db79ce560a2ee8950d14cdad239468a83
Tony Contreras <tony.contreras@gmail.com>
2013-04-28 00:25:15 -0400

    js push

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,6 @@
 //= require jquery
 //= require jquery_ujs
-//= require 'bootstrap/js/bootstrap.min.js'
+//= require 'bootstrap/js/bootstrap.js'
 //= require 'moment.js'
 //= require 'sugar.js'
 //= require 'underscore.js'
