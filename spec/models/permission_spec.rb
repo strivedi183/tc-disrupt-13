af8396f54c8345bd8bc98da855100a52066b7893
Sandip Trivedi <trivedi.sandip@gmail.com>
2013-04-29 18:49:02 -0400

    Set up testing infrastructure

---
+++ spec/models/permission_spec.rb
@@ -0,0 +1,12 @@
+# == Schema Information
+#
+# Table name: permissions
+#
+#  id         :integer          not null, primary key
+#  network    :string(255)
+#  handle     :string(255)
+#  event_id   :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
